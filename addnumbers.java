2f84b95da4957090b3952f4c5343ccb69ffe8305
Princy Selvarani A <162154321+PrincySelvarani@users.noreply.github.com>
2025-11-18 15:45:13 +0530

    Refactor addnumbers to use fixed values

--- addnumbers.java
+++ addnumbers.java
@@ -2,17 +2,11 @@ import java.util.Scanner;
 
 public class addnumbers {
     public static void main(String[] args) {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.print("Enter first number: ");
-        int a = sc.nextInt();
-
-        System.out.print("Enter second number: ");
-        int b = sc.nextInt();
-
+        int a = 10;
+        int b = 20;
         int sum = a + b;
-        System.out.println("Sum = " + sum);
 
-        sc.close();
+        System.out.println("Sum = " + sum);
     }
 }
+
